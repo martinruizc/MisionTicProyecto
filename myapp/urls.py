d2df099431f1d9beefc0f8a7c1e8a344614d9f82
Martin <martinyoruiz@gmail.com>
2022-09-12 21:45:53 -0500

    log in terminado

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
   path('instructor/', views.instructor, name="instructor"),
   path('create_instructor/', views.create_instructor, name="create_instructor"),
   path('instructor/<int:id>', views.instructor_detail, name='instructor_detail'),
-  path('instructor/<int:id>/delete/', views.delete_instrutor, name="delete_instructor")
+  path('signup/', views.sign_up, name="signup"),
+  path('signout/', views.signout, name="signout"),
+  path('signin/', views.signin, name="signin")
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,10 +1,59 @@
-from django.shortcuts import render, redirect,HttpResponse
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from .models import Instructor
 from .forms import InstructorForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
 
 # Create your views here.
 def main_page(request):
   return render(request, 'index.html')
+
+def sign_up(request):
+  if request.method == 'GET':
+    return render(request, 'signup.html', {
+      'form': UserCreationForm
+    })
+  else:
+    if request.POST['password1'] == request.POST['password2']:
+      try:
+        user = User.objects.create_user(username=request.POST['username'], password = request.POST['password1'])
+        user.save()
+        login(request, user)
+        return redirect('index')
+      except:
+        return render(request, 'signup.html',{
+          'form': UserCreationForm,
+          'error': 'username is alredy in used, try a new one'
+        })
+    else:
+      return render(request, 'signup.html', {
+        'form': UserCreationForm,
+        'error': 'Password does\'t match'
+      })
+
+def signout(request):
+  logout(request)
+  return redirect('index')      
+
+def signin(request):
+  if request.method == 'GET':
+    return render(request, 'signin.html', {
+      'form': AuthenticationForm
+    })
+  else:
+    user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+    
+    if user is None:
+      return render(request, 'signin.html', {
+        'form': AuthenticationForm,
+        'error': 'Invalid credentials'
+      })
+    else:
+      login(request, user)
+      return redirect('index')
+      
 
 def instructor(request):
   instructor = Instructor.objects.all()
@@ -36,15 +85,10 @@ def instructor_detail(request, id):
     #return redirect(f'instructor')
     return redirect('instructor')
 
-# def delete_instrutor(request, id):
-#   instructor = Instructor.objects.get(id=id)
-#   if request.method == 'POST':
-#     instructor.delete()
-#     return redirect('instructor')
 
 
 
-    
+
     
   
   
