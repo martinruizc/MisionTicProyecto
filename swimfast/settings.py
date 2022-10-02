0aca658db8a36d4c5a3727ba3df44b723e4a73d0
Martin <martinyoruiz@gmail.com>
2022-10-02 12:44:57 -0500

    whitenoise typo

--- swimfast/settings.py
+++ swimfast/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenosie.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'swimfast.urls'
